1d0507c87538e3ecca8e3a013206ee450f79a4a9
Alfie Jones <alfie.jones@hotmail.co.uk>
2021-07-11 02:27:06 +0100

    Update documentation.tsx

--- site/pages/documentation.tsx
+++ site/pages/documentation.tsx
@@ -3,5 +3,5 @@ import Image from "next/image"
 import styles from "../styles/Home.module.css"
 
 export default function Home() {
-  return <div className="text-center">Documentation Page</div>
+  return <div className="text-center">Documentation</div>
 }
