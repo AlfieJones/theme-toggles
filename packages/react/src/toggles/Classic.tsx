603542437995d5c237ab684eb11998502288e63e
Alfie Jones <32960904+AlfieJones@users.noreply.github.com>
2022-03-18 12:20:51 +0000

    Moved to translate3d

--- packages/react/src/toggles/Classic.tsx
+++ packages/react/src/toggles/Classic.tsx
@@ -28,7 +28,7 @@ const SvgClassic = forwardRef<HTMLButtonElement, ToggleProps>((props: ToggleProp
   };
 
   return <button ref={ref} className={btnClass} style={btnStyle} aria-label={ariaLabel} onClick={handleClick} {...rest}>
-            {<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" strokeLinecap="round" className="theme-toggle__classic" viewBox="0 0 32 32"><mask id="theme-toggle__classic__cutout" color="#000"><path fill="#fff" d="M0 0h32v32H0z" /><circle cx={34} cy={2} r={8} /></mask><circle cx={16} cy={16} r={9.34} mask="url(#theme-toggle__classic__cutout)" /><g stroke="currentColor" className="theme-toggle__classic__sun-rays" mask="url(#a)"><path d="M16 5.5v-4" /><path d="M16 30.5v-4" /><path d="M1.5 16h4" /><path d="M26.5 16h4" /><path d="m23.4 8.6 2.8-2.8" /><path d="m5.7 26.3 2.9-2.9" /><path d="m5.8 5.8 2.8 2.8" /><path d="m23.4 23.4 2.9 2.9" /></g></svg>}
+            {<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" strokeLinecap="round" className="theme-toggle__classic" viewBox="0 0 32 32"><mask id="theme-toggle__classic__cutout" color="#000"><path fill="#fff" d="M0 0h32v32H0z" /><circle cx={34} cy={2} r={8} /></mask><circle cx={16} cy={16} r={9.34} mask="url(#theme-toggle__classic__cutout)" /><g stroke="currentColor" className="theme-toggle__classic__sun-rays" mask="url(#theme-toggle__classic__cutout)"><path d="M16 5.5v-4" /><path d="M16 30.5v-4" /><path d="M1.5 16h4" /><path d="M26.5 16h4" /><path d="m23.4 8.6 2.8-2.8" /><path d="m5.7 26.3 2.9-2.9" /><path d="m5.8 5.8 2.8 2.8" /><path d="m23.4 23.4 2.9 2.9" /></g></svg>}
           </button>;
 });
 export default SvgClassic;
